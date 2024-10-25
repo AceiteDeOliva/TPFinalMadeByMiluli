b345fd6fbb81bcb219697a475a0ee33899104c31
milir99 <milagrosabril.r@gmail.com>
2024-10-25 16:09:48 -0300

    cree la nav bar
    cree carpetas y empecé el código para la barra de navegación

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarPrincipalComponent } from "./components/navbar_principal/navbar-principal/navbar-principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarPrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ e-commerce/src/app/components/navbar_principal/navbar-principal/navbar-principal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-principal',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-principal.component.html',
+  styleUrl: './navbar-principal.component.css'
+})
+export class NavbarPrincipalComponent {
+
+}
