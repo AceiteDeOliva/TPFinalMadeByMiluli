301ecff7a0185cce8be69e99d8c37b0022e1be84
Miluli <109126977+milir99@users.noreply.github.com>
2024-10-29 02:09:17 -0300

    creacion y estilo
    cree lanav bar,realice el css para que se vea como en figma, hice el routing de los elementos que lo necesitaban.

--- e-commerce/src/app/app.routes.ts
+++ e-commerce/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CarritoComponent } from './component/carrito/carrito.component';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  {path:'carrito', component:CarritoComponent},
+  {path:'home', component:HomeComponent}
+  ,{path:"**",redirectTo:"home"}
+];
