05be76f57632caa32374ecf6ea6d38fda4d5ce8b
AceiteDeOliva <josevl@live.com>
2024-10-30 17:28:29 -0300

    Funciones user service
    Se extrajo logica de los componentes de register y log in hacia user service para separar mejor las responsabilidades

--- e-commerce/src/app/component/login-form/login-form.component.ts
+++ e-commerce/src/app/component/login-form/login-form.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-login-form',
@@ -21,29 +21,27 @@ export class LoginFormComponent {
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private http: HttpClient
+    private userService: UserService
   ) {
-    // Initialize form group with controls for email and password
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
     });
   }
 
-  login() {
-    if (this.loginForm.invalid) { //checks that all fields are filled
-      alert('Please fill in all required fields.');
-      return;
-    }
-
-    const { email, password } = this.loginForm.value;
-
-    this.authenticateUser(email!, password!) // checks credentials
-      .pipe(
+    login() {
+      if (this.loginForm.invalid) {
+        alert('Please fill in all required fields.');
+        return;
+      }
+  
+      const { email, password } = this.loginForm.value;
+  
+      this.userService.authenticateUser(email!, password!).pipe(
         tap((isValid) => {
           if (isValid) {
             alert('Login successful!');
-            this.router.navigate(['/home']); //if credentials are valid it redirects to home
+            this.router.navigate(['/home']);
           } else {
             alert('Invalid email or password');
           }
@@ -51,17 +49,8 @@ export class LoginFormComponent {
         catchError((error) => {
           console.error('Login error', error);
           alert('An error occurred while logging in.');
-          return of(false); 
+          return of(false);
         })
-      )
-      .subscribe();
-  }
-
-  private authenticateUser(email: string, password: string): Observable<boolean> { // checks if the user && password combination exists
-    return this.http
-      .get<any[]>(`http://localhost:3000/users?email=${email}&password=${password}`)
-      .pipe(
-        map((users) => users.length > 0) 
-      );
-  }
+      ).subscribe();
+    }
 }
--- e-commerce/src/app/models/user.ts
+++ e-commerce/src/app/models/user.ts
@@ -1,2 +1,7 @@
 export interface User {
+    email: string;
+    password: string;
+    cart: Array<{ productId: string; quantity: number }>;
+    purchaseHistory: Array<{ productId: string; quantity: number; date: Date }>;
+    credential: 'user' | 'employee'|'manager'|'admin';
 }
