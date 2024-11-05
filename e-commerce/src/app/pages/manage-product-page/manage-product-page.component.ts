09fe7cd2f002c291322c2489fc9b0ceabe7ca526
milir99 <milagrosabril.r@gmail.com>
2024-11-05 02:04:48 -0300

    Agregar,listar y filtrar productos
    uni y cree componentes para ralizar abm admeas cambie algunas cosas visuales

---
+++ e-commerce/src/app/pages/manage-product-page/manage-product-page.component.ts
@@ -0,0 +1,19 @@
+import { ProductService } from './../../services/product-service/product.service';
+import { Component } from '@angular/core';
+import { ProductListComponent } from "../../component/product-list/product-list.component";
+import { Product } from '../../models/product';
+import { FormGroup,FormBuilder, ReactiveFormsModule,Validators} from '@angular/forms';
+import { ManageProductsComponent } from '../../component/manage-products/manage-products.component';
+import { RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-manage-product-page',
+  standalone: true,
+  imports: [ProductListComponent,ReactiveFormsModule,ManageProductsComponent,RouterModule,RouterOutlet],
+  templateUrl: './manage-product-page.component.html',
+  styleUrl: './manage-product-page.component.css'
+})
+export class ManageProductPageComponent {
+ 
+
+}
