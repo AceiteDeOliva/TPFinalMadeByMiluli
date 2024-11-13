90dfa9416ad9e8bd33dce35d2b627435619e6d3d
Miluli <109126977+milir99@users.noreply.github.com>
2024-11-12 23:30:18 -0300

    Revert "layou de home"
    This reverts commit bf13446bbeacb5795e2da3139adc1e9fa6df2303.

--- e-commerce/src/app/pages/home/home.component.ts
+++ e-commerce/src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "../../component/nav-bar/nav-bar.component";
+import { ProductSliderComponent } from "../../component/product-slider/product-slider.component";
+import { HomeSelectionsComponent } from "../../component/home-selections/home-selections.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductSliderComponent, HomeSelectionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
